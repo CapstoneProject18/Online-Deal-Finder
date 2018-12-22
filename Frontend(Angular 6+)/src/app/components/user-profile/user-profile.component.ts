e4bb3b129eca48860fd13d8cc3602bfca2d204a4
Tushar Sharma <tushar.ecmc@gmail.com>
2018-12-22 11:44:33 +0530

    Add files via upload

---
+++ Frontend(Angular 6+)/src/app/components/user-profile/user-profile.component.ts
@@ -0,0 +1,90 @@
+import {  Component, OnInit, ElementRef } from '@angular/core';
+import {NgbModal, ModalDismissReasons }from '@ng-bootstrap/ng-bootstrap';
+import { Router} from '@angular/router';
+import { DataService } from '../../provider/data.service';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+
+  styles: any[] = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}];
+    data : Date = new Date();
+    focus;
+    focus1;
+    closeResult: string;
+    selectedFile: FileList;
+    addProduct :FormGroup;
+    constructor(private modalService: NgbModal, private router:Router, public dat:DataService, private elem:ElementRef )  { }
+
+    createFormGroup() {
+        return new FormGroup({
+            pName:new FormControl(),
+            pPrice:new FormControl()
+        });
+      }
+
+    ngOnInit() {
+        this.addProduct = this.createFormGroup();
+
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.add('profile-page');
+        var navbar = document.getElementsByTagName('nav')[0];
+        navbar.classList.add('navbar-transparent');
+    }
+    ngOnDestroy(){
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.remove('profile-page');
+        var navbar = document.getElementsByTagName('nav')[0];
+        navbar.classList.remove('navbar-transparent');
+    }
+
+    open(content, type) {
+        if (type === 'sm') {
+            console.log('aici');
+            this.modalService.open(content, { size: 'sm' }).result.then((result) => {
+                this.closeResult = `Closed with: ${result}`;
+            }, (reason) => {
+                this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+            });
+        } else {
+            this.modalService.open(content).result.then((result) => {
+                this.closeResult = `Closed with: ${result}`;
+            }, (reason) => {
+                this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+            });
+        }
+    }
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return  `with: ${reason}`;
+        }
+    }
+
+    navigate()
+    {
+        this.router.navigate(['pages/cart']);
+    }
+    addItem(){
+    console.log(this.addProduct);
+    this.dat.addItemToDatabase(this.addProduct.value, this.selectedFile.item(0))
+      .subscribe(response =>{
+        console.log(response);
+        alert('success');
+      }, error => {
+        console.log("error!!");
+      });
+    }
+
+    selectFile(event) {
+        this.selectedFile = event.target.files;
+      }
+}
